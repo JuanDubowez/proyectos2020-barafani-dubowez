931b0cd9c2da44d7ee576f75232cef3b1e994ea0
Facundo Barafani <facu1910@gmail.com>
2020-04-15 11:32:08 -0300

    Adjust layout

---
+++ js/canvas.js
@@ -0,0 +1,6 @@
+var canvas1 = document.getElementById("canvas1");
+var c = canvas1.getContext("2d");
+c.fillStyle = "#FF0000";
+c.fillRect(0, 0, 20, 25);
+c.font = "30px Arial";
+c.fillText("1", 10, 50);
