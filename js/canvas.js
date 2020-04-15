eaa2276c39474eddc597ce431b1eb0f9f60d4d71
Facundo Barafani <facu1910@gmail.com>
2020-04-15 15:25:16 -0300

    Add function to draw in canvas

--- js/canvas.js
+++ js/canvas.js
@@ -1,25 +1,21 @@
 // Accedemos a componentes html desde el archivo JS
 var createMatrixModalBody = document.getElementById("matrix-modal-body");
-// Creamos la matriz en el canvas
-
 var canvas1 = document.getElementById("canvas1");
 var c = canvas1.getContext("2d");
-c.fillStyle = "#FF0000";
-c.fillRect(0, 0, 20, 25);
-c.font = "30px Arial";
-c.fillText("1", 10, 50);
 
-for (var i=0;i<5;i++){
-    c.fillText("1",10+(i*20),50);
-}
+var rowValue;
+var columnValue;
+
 
 // Funcion para obtener las filas y columnas de una matriz
 
 function getMatrixValues(){
-    var rowValue = document.getElementById("rowTextField").value;
-    var columnValue = document.getElementById("columnTextField").value;
+    rowValue = document.getElementById("rowTextField").value;
+    columnValue = document.getElementById("columnTextField").value;
     createMatrixInputs(rowValue,columnValue);
 }
+
+// Funcion para crear los inputs segun la cantidad de filas y columnas de la matriz
 
 function createMatrixInputs(row,column){
     for (var i = 0; i< row; i++){
@@ -31,6 +27,31 @@ function createMatrixInputs(row,column){
             var matrixInput = document.createElement('input');
             var matrix = document.getElementById('matrixRow'+(i+1));
             matrix.appendChild(matrixInput);
+        }
+    }
+}
+
+// Funcion para borrar los componentes del modal creado
+
+function deleteMatrixInputs(){
+    createMatrixModalBody.innerHTML = "";
+}
+
+// Creamos la matriz en el canvas
+c.fillStyle = "#FF0000";
+c.fillRect(0, 0, 20, 25);
+c.font = "30px Arial";
+c.fillText("1", 10, 50);
+
+
+// Funcion para dibujar la matriz en el canvas
+
+function drawMatrix(){
+    console.log(rowValue);
+    for (var i=0; i<rowValue;i++){
+        for (var j=0;j<columnValue;j++){
+            c.font = "30px Arial";
+            c.fillText('1',10+(j*20), 50+(j*20))
         }
     }
 }
