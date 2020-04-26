54582cace9dd39a64559ad74abab1bd18d083c11
Facundo Barafani <facu1910@gmail.com>
2020-04-26 16:32:32 -0300

    Add function to get the reduced matrix

--- js/canvas.js
+++ js/canvas.js
@@ -24,11 +24,11 @@ function createMatrixInputs(row,column){
     for (var i = 0; i< row; i++){
             var matrixRow = document.createElement('div');
             matrixRow.classList = 'row m-2 justify-content-center'
-            matrixRow.setAttribute('id','matrixRow'+(i+1))
+            matrixRow.setAttribute('id','matrixRow'+(i+1));
             createMatrixModalBody.appendChild(matrixRow);
         for (var j = 0; j<column; j++) {
             var matrixInput = document.createElement('input');
-            matrixInput.setAttribute('id','input-'+(i+1)+'_'+(j+1))
+            matrixInput.setAttribute('id','input-'+(i+1)+'_'+(j+1));
             var matrix = document.getElementById('matrixRow'+(i+1));
             matrix.appendChild(matrixInput);
         }
@@ -40,11 +40,12 @@ function obtainInputValues(){
     for (var i=0;i<rowValue;i++){
         var row=[];
         for (var j=0;j<columnValue;j++){
-            row.push(document.getElementById('input-'+(i+1)+'_'+(j+1)).value)
+            row.push(document.getElementById('input-'+(i+1)+'_'+(j+1)).value);
         }
         matrix.push(row);
     }
-    drawMatrix(matrix);
+    drawMatrix(matrix,c);
+    reduceMatrix(matrix);
 }
 
 // Funcion para borrar los componentes del modal creado
@@ -55,22 +56,63 @@ function deleteMatrixInputs(){
 
 // Funcion para dibujar la matriz en el canvas
 
-function drawMatrix(matrix){
+function drawMatrix(matrix,context){
     console.log(matrix.length);
-    for (var i=0; i<(matrix.length);i++){
-        for (var j=0; j<(matrix.length);j++){
+    for (var i=0; i<(rowValue);i++){
+        for (var j=0; j<(columnValue);j++){
             console.log(matrix[i][j]);
-            c.beginPath();
-            c.moveTo(0,0)
-            c.lineTo(0,i*100)
-            c.stroke()
-            c.beginPath();
-            c.moveTo(300,0)
-            c.lineTo(300,i*100)
-            c.stroke()
-            c.lineWidth = 5
-            c.font = "30px Arial";
-            c.fillText(matrix[i][j],10+(j*100), 50+(30*i))
+            context.font = "30px Arial";
+            context.fillText(matrix[i][j],80+(j*100), 50+(30*i));
         }
     }
+    context.lineWidth = 4
+    context.beginPath();
+    context.moveTo(0,0);
+    context.lineTo(0,i*100);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(300,0);
+    context.lineTo(300,i*100);
+    context.stroke();
+}
+
+// Funcion para calcular la matriz reducida por filas
+
+function reduceMatrix(matrix){
+    var lead = 0;
+    for (var r = 0; r < rowValue; r++) {
+        if (columnValue <= lead) {
+            return;
+        }
+        var i = r;
+        while (matrix[i][lead] == 0) {
+            i++;
+            if (rowValue == i) {
+                i = r;
+                lead++;
+                if (columnValue == lead) {
+                    return;
+                }
+            }
+        }
+ 
+        var tmp = matrix[i];
+        matrix[i] = matrix[r];
+        matrix[r] = tmp;
+ 
+        var val = matrix[r][lead];
+        for (var j = 0; j < columnValue; j++) {
+            matrix[r][j] /= val;
+        }
+ 
+        for (var i = 0; i < rowValue; i++) {
+            if (i == r) continue;
+            val = matrix[i][lead];
+            for (var j = 0; j < columnValue; j++) {
+                matrix[i][j] -= val * matrix[r][j];
+            }
+        }
+        lead++;
+    }
+    drawMatrix(matrix,c2);
 }
